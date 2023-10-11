d2009fd04da9c263f427d6b3c670a6289c46ed38
sung jun <chosung0531@naver.com>
2023-10-11 16:21:01 +0900

    몬스터 패트롤 기능 추가
    몬스터의 패트롤 기능을 추가해서 계속 돌아다니게 구현

--- Assets/Scripts/Monster/BaseController.cs
+++ Assets/Scripts/Monster/BaseController.cs
@@ -4,24 +4,47 @@ using UnityEngine;
 
 public abstract class BaseController : MonoBehaviour
 {
-    [SerializeField] protected GameObject player;
+    protected GameObject player;
     protected bool chasePlayer = false;
     protected LayerMask playerMask =1<<3;
-    
+
     [Header("이형체 변수")]
     [SerializeField] protected float moveSpeed;
+    [SerializeField] protected float chaseSpeed;
     [SerializeField] protected float detectDist;
     [SerializeField] protected float maxChaseDist;
+    [SerializeField] protected float attackRange;
+    [SerializeField] protected bool patrolMonster = false;
     [SerializeField] protected AnimationState monsterState = AnimationState.Idle;
+
+    public virtual AnimationState State
+    {
+        get { return monsterState; }
+        set
+        {
+            //Animator anim = GetComponent<Animator>();
+            monsterState = value;
+            switch (monsterState)
+            {
+                case AnimationState.Idle:
+                    //anim.CrossFade("Idle", 0.1f);
+                    break;
+                case AnimationState.Chase:
+                    //anim.CrossFade("Run", 0.1f);
+                    break;
+                case AnimationState.Attack:
+                    //anim.CrossFade("Attack", 0.1f, -1, 0);
+                    break;
+            }
+        }
+    }
 
     void Start(){ Init();}
 
     void Update()
     {
-        DetectPlayer();
         switch (monsterState)
         {
-            //anim.CrossFade("Idle", 0.1f); 애니메이션 이름
             case AnimationState.Idle:
                 UpdateIdle();
                 break;
@@ -35,32 +58,12 @@ public abstract class BaseController : MonoBehaviour
     }
 
     public abstract void Init();
-    protected virtual void DetectPlayer() { }
+    protected virtual void Patroll() { }
     protected virtual void UpdateIdle() { }
     protected virtual void UpdateChase() { }
     protected virtual void UpdateAttack() { }
     
 }
 
-/*public virtual AnimationState State
-{
-    get { return playerState; }
-    set
-    {
-        Animator anim = GetComponent<Animator>();
-        playerState = value;
-        switch (playerState)
-        {
-            case AnimationState.Idle:
-                anim.CrossFade("Idle", 0.1f);
-                break;
-            case AnimationState.Walk:
-                anim.CrossFade("Run", 0.1f);
-                break;
-            case AnimationState.Attack:
-                anim.CrossFade("Attack", 0.1f, -1, 0);
-                break;
-        }
-    }
-}*/
+
 
