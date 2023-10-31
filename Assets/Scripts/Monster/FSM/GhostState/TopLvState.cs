15612eb9c0a5382c3c7ad1623b787c89a001bbb4
sung jun <chosung0531@naver.com>
2023-10-31 16:48:14 +0900

    이형체들의 뼈대 코드 작성
    각 단계의 이형체들 뼈대 코드 작성 완료
    (하위, 중위, 상위, 최상위)

--- Assets/Scripts/Monster/FSM/GhostState/TopLvState.cs
+++ Assets/Scripts/Monster/FSM/GhostState/TopLvState.cs
@@ -2,85 +2,84 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace LowerLevelStates
+namespace TopLevelStates
 {
-    public class Indifference : State<LowerLv>
+    public class Indifference : State<TopLv>
     {
-        public override void Enter(LowerLv entity)
+        public override void Enter(TopLv entity)
         {
             Debug.Log("무관심 상태이다.");
         }
 
-        public override void Execute(LowerLv entity)
+        public override void Execute(TopLv entity)
         {
             Debug.Log("계속 무관심하다.");
             if (entity.DetectPlayer())
                 entity.ChangeState(EntityStates.Watch);
         }
 
-        public override void Exit(LowerLv entity)
+        public override void Exit(TopLv entity)
         {
             Debug.Log("무관심 상태가 아니다.");
         }
     }
-    public class Watch : State<LowerLv>
+    public class Watch : State<TopLv>
     {
-        public override void Enter(LowerLv entity)
+        public override void Enter(TopLv entity)
         {
             Debug.Log("관찰 상태이다.");
             entity.transform.LookAt(entity.playerObject.transform);
         }
 
-        public override void Execute(LowerLv entity)
+        public override void Execute(TopLv entity)
         {
             Debug.Log("계속 관찰한다.");
             if (!entity.CheckDistance())
                 entity.ChangeState(EntityStates.Indifference);
         }
 
-        public override void Exit(LowerLv entity)
+        public override void Exit(TopLv entity)
         {
             Debug.Log("관찰 상태가 아니다.");
         }
     }
 
-    public class Chase : State<LowerLv>
+    public class Chase : State<TopLv>
     {
-        public override void Enter(LowerLv entity)
+        public override void Enter(TopLv entity)
         {
             Debug.Log("쫓는 상태이다.");
             entity.Speed = entity.chaseSpeed;
         }
 
-        public override void Execute(LowerLv entity)
+        public override void Execute(TopLv entity)
         {
             Debug.Log("계속 쫓는중이다.");
             entity.ChasePlayer();
         }
 
-        public override void Exit(LowerLv entity)
+        public override void Exit(TopLv entity)
         {
             Debug.Log("더이상 쫓지 않는다.");
         }
     }
 
-    public class Patrol : State<LowerLv>
+    public class Patrol : State<TopLv>
     {
-        public override void Enter(LowerLv entity)
+        public override void Enter(TopLv entity)
         {
             Debug.Log("순찰 상태이다.");
             entity.Speed = entity.patrolSpeed;
         }
 
-        public override void Execute(LowerLv entity)
+        public override void Execute(TopLv entity)
         {
             Debug.Log("계속 순찰중이다.");
         }
 
-        public override void Exit(LowerLv entity)
+        public override void Exit(TopLv entity)
         {
             Debug.Log("더이상 순찰 상태가 아니다.");
         }
     }
 }
-
