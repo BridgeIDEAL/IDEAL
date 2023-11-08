6eeec9fc1f90da4efdf61cf86ac6cde0f18fa144
hch3245 <hch3245@naver.com>
2023-11-09 00:45:40 +0900

    Add: Inventory Test Data Add

---
+++ Assets/Scripts/Interaction/Inventory/InteractionGetPortion_101.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractionGetPortion_101 : AbstractInteraction
+{
+    [SerializeField] PortionItemData portionItemData;
+
+    protected override string GetDetectedString(){
+        return "Press E, Get Portion!";
+    }
+
+    protected override void ActInteraction(){
+        Inventory.Instance.Add(portionItemData, 1);
+    }
+}
