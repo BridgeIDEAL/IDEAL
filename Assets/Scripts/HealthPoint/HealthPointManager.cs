6c9bddd4ccf8ebd424e52cf7c6f158d4c32a3f3c
hch3245 <hch3245@naver.com>
2023-11-18 21:00:22 +0900

    Add: Health Point System(Need Health Effect)

---
+++ Assets/Scripts/HealthPoint/HealthPointManager.cs
@@ -0,0 +1,108 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum IdealBodyPart{
+    Head = 0,
+    Torso,
+    LeftArm,
+    RightArm,
+    LeftLeg,
+    RightLeg
+}
+
+public class HealthPointManager : MonoBehaviour
+{
+    private static HealthPointManager instance = null;
+    public static HealthPointManager Instance{
+        get{
+            if(instance == null) return null;
+            return instance;
+        }
+    }
+
+    // 2 == 정상, 1 == 손상,  0 == 제거
+    private int[] healthPoint = new int[System.Enum.GetValues(typeof(IdealBodyPart)).Length];
+
+    private int maxHP = 2;
+    private int minHP = 0;
+
+    
+    void Awake(){
+        if(instance == null){
+            instance = this;
+        }
+        else{
+            Destroy(this.gameObject);
+        }
+
+        Init();
+    }
+
+    public void Init(){
+        for(int i = 0; i < healthPoint.Length; i++){
+            healthPoint[i] = maxHP;
+        }
+    }
+    private int GetHealthPoint(IdealBodyPart idealBodyPart){
+        return healthPoint[(int)idealBodyPart];
+    }
+
+    public void Hurt(IdealBodyPart idealBodyPart, int damage){
+        int bodyNum = (int)idealBodyPart;
+        healthPoint[bodyNum] -= damage;
+        if(healthPoint[bodyNum] < minHP) healthPoint[bodyNum] = minHP;
+        UpdateHealthEffect(idealBodyPart, healthPoint[bodyNum]);
+    }
+
+    public bool Heal(IdealBodyPart idealBodyPart, int repair){
+        int bodyNum = (int)idealBodyPart;
+        if(healthPoint[bodyNum] > minHP){
+            healthPoint[bodyNum] += repair;
+            if(healthPoint[bodyNum] > maxHP) healthPoint[bodyNum] = maxHP;
+            UpdateHealthEffect(idealBodyPart, healthPoint[bodyNum]);
+            return true;
+        }
+        else{
+            UpdateHealthEffect(idealBodyPart, healthPoint[bodyNum]);
+            return false;
+        }
+    }
+
+    private void UpdateHealthEffect(IdealBodyPart idealBodyPart, int hp){
+        switch(idealBodyPart){
+            case IdealBodyPart.Head:
+                // TO DO
+                // 머리의 체력에 따라 나타나는 현상
+                Debug.Log($"Head HP :{hp}");
+                break;
+            case IdealBodyPart.Torso:
+                // TO DO
+                // 몸통의 체력에 따라 나타나는 현상
+                Debug.Log($"Torso HP :{hp}");
+                break;
+            case IdealBodyPart.LeftArm:
+                // TO DO
+                // 왼쪽 팔의 체력에 따라 나타나는 현상
+                Debug.Log($"LeftArm HP :{hp}");
+                break;
+            case IdealBodyPart.RightArm:
+                // TO DO
+                // 오른쪽 팔의 체력에 따라 나타나는 현상
+                Debug.Log($"RightArm HP :{hp}");
+                break;
+            case IdealBodyPart.LeftLeg:
+                // TO DO
+                // 왼쪽 다리의 체력에 따라 나타나는 현상
+                Debug.Log($"LeftLeg HP :{hp}");
+                break;
+            case IdealBodyPart.RightLeg:
+                // TO DO
+                // 오른쪽 다리의 체력에 따라 나타나는 현상
+                Debug.Log($"RightLeg HP :{hp}");
+                break;
+            default:
+                break;
+        }
+    }
+}
