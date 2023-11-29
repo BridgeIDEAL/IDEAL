34eb7ad32631fb110228fa25b9fffb070d8a4c83
sung jun <chosung0531@naver.com>
2023-11-29 14:48:46 +0900

    conflict 에러 해결

--- Assets/Scripts/Environment/GameScene.cs
+++ Assets/Scripts/Environment/GameScene.cs
@@ -10,6 +10,6 @@ public class GameScene : MonoBehaviour
     }
     public void Init()
     {
-        GameManager.Sound.PlaySound("ClassRoomWav",SoundType.Ambience);
+        //GameManager.Sound.PlaySound("ClassRoomWav",SoundType.Ambience);
     }
 }
