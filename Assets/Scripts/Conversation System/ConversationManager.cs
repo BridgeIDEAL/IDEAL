02489120e7d13b6ce8ea46be28b4fcb74ee31de6
hch3245 <hch3245@naver.com>
2023-12-01 03:04:43 +0900

    Add: 게임 오버 구현과 대화 스크립트 개선

--- Assets/Scripts/Conversation System/ConversationManager.cs
+++ Assets/Scripts/Conversation System/ConversationManager.cs
@@ -47,11 +47,55 @@ public class ConversationManager : MonoBehaviour
     #endregion
 
     #region Interaction
+    // Yarn Spinner에서 매개변수 받는 것을 사이트와 동일하게 했지만 전혀 통하지 않음! 으악
+    // 일단 노가다로 해결하고 이후 해결방법을 고려해보도록 하겠음
+    
     [YarnCommand("GameOver")]
     public void GameOver(){
-        // TO DO
-        // 게임 오버 처리 필요
-        Debug.Log("======== Game Over ========");
+        int attempts = CountAttempts.Instance.GetAttemptCount();
+        GameOverManager.Instance.GameOver($"{attempts}번 실종자는 ...");
+    }
+    
+    [YarnCommand("GameOver_1")]
+    public void GameOver_1(){
+        int attempts = CountAttempts.Instance.GetAttemptCount();
+        GameOverManager.Instance.GameOver($"이후 끌려간 {attempts}번 실종자는 학교 구석에서 온몸이 짓뭉개진 채 발견됨");
+    }
+
+    [YarnCommand("GameOver_2")]
+    public void GameOver_2(){
+        int attempts = CountAttempts.Instance.GetAttemptCount();
+        GameOverManager.Instance.GameOver($"{attempts}번 실종자는 수위실에 들어간 후 통신이 끊김");
+    }
+
+    [YarnCommand("GameOver_3")]
+    public void GameOver_3(){
+        int attempts = CountAttempts.Instance.GetAttemptCount();
+        GameOverManager.Instance.GameOver($"이후 수위 이형체가 {attempts}번 실종자를 끌고 간 후 통신이 끊김");
+    }
+
+    [YarnCommand("GameOver_4")]
+    public void GameOver_4(){
+        int attempts = CountAttempts.Instance.GetAttemptCount();
+        GameOverManager.Instance.GameOver($"이후 {attempts}번 실종자와 통신이 끊김");
+    }
+
+    [YarnCommand("GameOver_5")]
+    public void GameOver_5(){
+        int attempts = CountAttempts.Instance.GetAttemptCount();
+        GameOverManager.Instance.GameOver($"이후 학생회장은 {attempts}번 실종자로 ???를 만들어 학생회실 내부에 전시");
+    }
+    
+    [YarnCommand("GameOver_6")]
+    public void GameOver_6(){
+        int attempts = CountAttempts.Instance.GetAttemptCount();
+        GameOverManager.Instance.GameOver($"{attempts}번 실종자는 가입 이후 매우 즐거워하며 탈출을 포기. 이후 학교 내부에서 목격 증언이 다수 발생");
+    }
+
+    [YarnCommand("GameOver_7")]
+    public void GameOver_7(){
+        int attempts = CountAttempts.Instance.GetAttemptCount();
+        GameOverManager.Instance.GameOver($"이후 {attempts}번 실종자는 수색 중 잠든 채로 발견. 현재 157시간 동안 눈을 뜨지 않음");
     }
 
     [YarnCommand("ChalkBoardSuccess")]
@@ -61,6 +105,10 @@ public class ConversationManager : MonoBehaviour
         Debug.Log("======== Success ChalkBoard");
     }
 
+    [YarnCommand("GetAttemptsCount")]
+    public int GetAttemptsCount(){
+        return CountAttempts.Instance.GetAttemptCount();
+    }
     #endregion
 
     #region HurtBodyPart
@@ -71,6 +119,26 @@ public class ConversationManager : MonoBehaviour
         }
         else{
             HealthPointManager.Instance.Hurt(IdealBodyPart.RightArm, 1);
+        }
+    }
+
+    [YarnCommand("HealArm")]
+    public void HealArm(){
+        if(HealthPointManager.Instance.GetHealthPoint(IdealBodyPart.LeftArm) < HealthPointManager.maxHP){
+            HealthPointManager.Instance.Heal(IdealBodyPart.LeftArm, 1);
+        }
+        else{
+            HealthPointManager.Instance.Heal(IdealBodyPart.RightArm, 1);
+        }
+    }
+
+    [YarnCommand("HealLeg")]
+    public void HealLeg(){
+        if(HealthPointManager.Instance.GetHealthPoint(IdealBodyPart.LeftLeg) < HealthPointManager.maxHP){
+            HealthPointManager.Instance.Heal(IdealBodyPart.LeftLeg, 1);
+        }
+        else{
+            HealthPointManager.Instance.Heal(IdealBodyPart.RightLeg, 1);
         }
     }
 
@@ -105,6 +173,15 @@ public class ConversationManager : MonoBehaviour
     public void HurtHead(){
         HealthPointManager.Instance.Hurt(IdealBodyPart.Head, 1);
     }
+
+    #endregion
+
+    #region Activation Log
+    [YarnCommand("HurtBothArmLog")]
+    public void HurtBothArmLog(){
+        ActivationLogManager.Instance.AddActivationLog(1002);
+    }
+
 
     #endregion
 
---
+++ Assets/Scripts/Scene Manage/GameOverManager.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameOverManager : MonoBehaviour
+{
+    private static GameOverManager instance = null;
+    public static GameOverManager Instance{
+        get{
+            if(instance == null) return null;
+            return instance;
+        }
+    }
+
+    [SerializeField] private GameObject backgroundObject;
+    [SerializeField] private GameObject endingMentObject;
+    [SerializeField] private GameObject pressKeyTextObject;
+    private float pressKeyTime = 1.0f;
+    private TextMeshProUGUI endingMentText;
+    private bool isEnd = false;
+    private float stepTimer = 0.0f;
+    
+
+    private void Awake(){
+        if(instance == null){
+            instance = this;
+        }
+        else{
+            Destroy(this.gameObject);
+        }
+        backgroundObject.SetActive(false);
+        endingMentObject.SetActive(false);
+        endingMentText = endingMentObject.GetComponent<TextMeshProUGUI>();
+        pressKeyTextObject.SetActive(false);
+    }
+
+    public void GameOver(string endingMent){
+        backgroundObject.SetActive(true);
+        endingMentText.text = endingMent;
+        endingMentObject.SetActive(true);
+        isEnd = true;
+        stepTimer = 0.0f;
+    }
+
+    private void Update(){
+        if(isEnd){
+            if(stepTimer >= pressKeyTime){
+                pressKeyTextObject.SetActive(true);
+                if(Input.anyKeyDown){
+                    IdealSceneManager.Instance.LoadLobbyScene();
+                }
+            }
+            stepTimer += Time.deltaTime;
+        }
+    }
+}
