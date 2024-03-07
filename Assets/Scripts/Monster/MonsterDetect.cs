a59a0f6806e8743e360b85692e0657ebc0b1c75f
hch3245 <hch3245@naver.com>
2024-03-08 00:25:51 +0900

    Fix: Monster Detect 필요없는 로그 삭제

--- Assets/Scripts/Monster/MonsterDetect.cs
+++ Assets/Scripts/Monster/MonsterDetect.cs
@@ -5,8 +5,6 @@ using UnityEngine;
 public class MonsterDetect : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other){
-        Debug.Log("trigger tag: " + other.gameObject.name);
-		Debug.Log("EntityEvent: " + GameManager.EntityEvent.IsChase);
         if (other.gameObject.CompareTag("Monster") && GameManager.EntityEvent.IsChase) {
 			GameOverManager.Instance.GameOver("학생에게 끌려간 후 실종됨.");
 		}
