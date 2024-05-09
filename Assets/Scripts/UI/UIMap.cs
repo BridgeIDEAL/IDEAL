e1c9685188a73c21dfd6ce11e299785374a61d58
hch3245 <hch3245@naver.com>
2024-05-09 23:35:47 +0900

    Add: 지도 기능 추가 / 배치 필요

--- Assets/Scripts/UI/UIMap.cs
+++ Assets/Scripts/UI/UIMap.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 
 public class UIMap : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    [SerializeField] private GameObject mapUIObject;
+    private int mapItemCode = 1106;
     void Update()
     {
-        
+        if(Inventory.Instance.FindItemIndex(mapItemCode) == -1){
+            mapUIObject.SetActive(false);
+        }
+        else{
+            mapUIObject.SetActive(true);
+        }
     }
 }
