6de9e3b90450b92b57464cdadbbc5d2532043b3b
Sung Jun Cho <chosung0531@naver.com>
2024-08-03 00:42:57 +0900

    방송실 소품 배치

--- Assets/Scripts/UI/Dialogue/TestPassword.cs
+++ Assets/Scripts/UI/Dialogue/TestPassword.cs
@@ -5,13 +5,13 @@ using UnityEngine;
 public class TestPassword : MonoBehaviour
 {
     
-    void Update()
-    {
-        if(Input.GetKeyDown(KeyCode.B))
-            DialogueManager.Instance.Password_UI.ActivePassword();
-        if (Input.GetKeyDown(KeyCode.Q))
-        {
-            DialogueManager.Instance.StartDialogue("ShopManager1", "ShopManager");
-        }
-    }
+    //void Update()
+    //{
+    //    if(Input.GetKeyDown(KeyCode.B))
+    //        DialogueManager.Instance.Password_UI.ActivePassword();
+    //    if (Input.GetKeyDown(KeyCode.Q))
+    //    {
+    //        DialogueManager.Instance.StartDialogue("ShopManager1", "ShopManager");
+    //    }
+    //}
 }
