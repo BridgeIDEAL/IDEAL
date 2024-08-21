0aa1b501b5da6b9db70ce754d34dc520520e5c6e
Sung Jun Cho <chosung0531@naver.com>
2024-08-21 16:50:36 +0900

    교실 리모델링 완료 (2)

--- Assets/Scripts/Utils/MeshCombiner.cs
+++ Assets/Scripts/Utils/MeshCombiner.cs
@@ -1,18 +1,49 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class MeshCombiner : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject Parent;
+    public Material Material;
+    public bool DeactivateParentAfterMerge = true;
+    public bool DestroyParentAfterMerge = false;
+
+    [ContextMenu("Merge")]
+    public void MergeMeshes()
     {
-        
+        MeshFilter[] meshFilters = Parent.GetComponentsInChildren<MeshFilter>();
+        List<CombineInstance> combineList = new List<CombineInstance>();
+
+        for (int i = 0; i < meshFilters.Length; i++)
+        {
+            if (meshFilters[i].sharedMesh != null)
+            {
+                CombineInstance combineInstance = new CombineInstance
+                {
+                    mesh = meshFilters[i].sharedMesh,
+                    transform = meshFilters[i].transform.localToWorldMatrix
+                };
+                combineList.Add(combineInstance);
+            }
+        }
+
+        GameObject combinedObject = new GameObject("Combined Mesh");
+        combinedObject.AddComponent<MeshFilter>();
+        combinedObject.AddComponent<MeshRenderer>();
+        combinedObject.GetComponent<MeshFilter>().sharedMesh = new Mesh();
+        combinedObject.GetComponent<MeshFilter>().sharedMesh.CombineMeshes(combineList.ToArray());
+        combinedObject.GetComponent<MeshRenderer>().material = Material;
+
+        if (DeactivateParentAfterMerge)
+        {
+            Parent.SetActive(false);
+        }
+
+        if (DestroyParentAfterMerge)
+        {
+            Destroy(Parent);
+        }
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
