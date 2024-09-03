677f7835e3a062560b1b30f817dad3b91642e06e
hch3245 <hch3245@gmail.com>
2024-09-03 11:46:53 +0900

    게시판 가이드 UI 한번에 6개 보이도록 변경

--- Assets/Scripts/UI/UIGuideBook.cs
+++ Assets/Scripts/UI/UIGuideBook.cs
@@ -5,26 +5,41 @@ using UnityEngine;
 public class UIGuideBook : MonoBehaviour
 {
     [SerializeField] private GameObject[] guideUIObjects;
-    [SerializeField] private GameObject changeMapButtonObjectLeft;
-    [SerializeField] private GameObject changeMapButtonObjectRight;
+    [SerializeField] private GameObject setGuideUIObject;
+    [SerializeField] private bool watchSetGuideUI = false;
+    [SerializeField] private GameObject changeGuideUIButtonObjectLeft;
+    [SerializeField] private GameObject changeGuideUIButtonObjectRight;
+    [SerializeField] private RectTransform closeGuideUIRectTransform;
 
     private int pageNum = 0;
 
 
     public void ActiveGuide(){
         // 테스트 코드
-        for(int i = 1; i < guideUIObjects.Length; i++){
+        for(int i = 0; i < guideUIObjects.Length; i++){
             guideUIObjects[i].SetActive(false);
         }
-        guideUIObjects[pageNum].SetActive(true);
-        changeMapButtonObjectLeft.SetActive(true);
-        changeMapButtonObjectRight.SetActive(true);
-        if(pageNum == 0){
-            changeMapButtonObjectLeft.SetActive(false);
+        setGuideUIObject.SetActive(false);
+        changeGuideUIButtonObjectLeft.SetActive(false);
+        changeGuideUIButtonObjectRight.SetActive(false);
+    
+        if(watchSetGuideUI){
+            setGuideUIObject.SetActive(true);
+            closeGuideUIRectTransform.anchoredPosition = new Vector2(763.2f, -398.9f);
         }
-        if(pageNum == guideUIObjects.Length -1){
-            changeMapButtonObjectRight.SetActive(false);
+        else{
+            guideUIObjects[pageNum].SetActive(true);
+            changeGuideUIButtonObjectLeft.SetActive(true);
+            changeGuideUIButtonObjectRight.SetActive(true);
+            if(pageNum == 0){
+                changeGuideUIButtonObjectLeft.SetActive(false);
+            }
+            if(pageNum == guideUIObjects.Length -1){
+                changeGuideUIButtonObjectRight.SetActive(false);
+            }
+            closeGuideUIRectTransform.anchoredPosition = new Vector2(573.3f, -454);
         }
+        
     }
 
     public void ChangeGuideUI(bool left){
@@ -38,13 +53,13 @@ public class UIGuideBook : MonoBehaviour
             guideUIObjects[i].SetActive(false);
         }
         guideUIObjects[pageNum].SetActive(true);
-        changeMapButtonObjectLeft.SetActive(true);
-        changeMapButtonObjectRight.SetActive(true);
+        changeGuideUIButtonObjectLeft.SetActive(true);
+        changeGuideUIButtonObjectRight.SetActive(true);
         if(pageNum == 0){
-            changeMapButtonObjectLeft.SetActive(false);
+            changeGuideUIButtonObjectLeft.SetActive(false);
         }
         if(pageNum == guideUIObjects.Length -1){
-            changeMapButtonObjectRight.SetActive(false);
+            changeGuideUIButtonObjectRight.SetActive(false);
         }
     }
 }
