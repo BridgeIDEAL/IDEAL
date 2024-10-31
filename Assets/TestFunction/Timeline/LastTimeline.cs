42198f0c63936e40fdaee85a6fcf92b4d2ccb216
Sung Jun Cho <chosung0531@naver.com>
2024-10-31 20:32:33 +0900

    타임라인 컷씬, 파일정리, 빌딩 모델링 문제 해결

--- Assets/TestFunction/Timeline/LastTimeline.cs
+++ Assets/TestFunction/Timeline/LastTimeline.cs
@@ -1,53 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Runtime.CompilerServices;
-using Unity.VisualScripting;
-using UnityEditor.PackageManager;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LastTimeline : MonoBehaviour
 {
     [SerializeField] Camera mainCam;
-    [SerializeField, Header("Last PrincipalMove")] Transform lastPrincipal;
-    Vector3 destPos = new Vector3(5.3f,0f,5f);
-    Vector3 destAngle = new Vector3(0, 200, 0);
-    public void CallMoveNRotate() { StartCoroutine(Move()); }
-
-    IEnumerator Move()
-    {
-        float timer = 0f;
-        Vector3 stPos = lastPrincipal.position;
-        while (timer < 1f)
-        {
-            timer += Time.deltaTime;
-            lastPrincipal.transform.position = Vector3.Lerp(stPos, destPos, timer / 1f);
-            yield return null;
-        }
-        lastPrincipal.transform.position = destPos;
-        StartCoroutine(Rotate());
-    }
-
-    IEnumerator Rotate()
-    {
-        float timer = 0f;
-        Quaternion stRot = lastPrincipal.transform.rotation;
-        Quaternion edRot = Quaternion.Euler(destAngle);
-        while (timer < 0.2f)
-        {
-            timer += Time.deltaTime;
-            lastPrincipal.transform.rotation = Quaternion.Slerp(stRot, edRot, timer / 1f);
-            yield return null;
-        }
-        lastPrincipal.transform.rotation = edRot;
-    }
-
+    [SerializeField] Image fadeImage;
+    [SerializeField] float decreaseView;
+    [SerializeField] float increaseView;
+    [SerializeField] float fadeoutTime;
+    
     public void CallDecreaseFieldOfView() { StartCoroutine(DecreaseFieldOfView()); }
     [SerializeField, Header("FieldView Time")] float decreaseFieldViewTime; 
     IEnumerator DecreaseFieldOfView()
     {
         float timer = 0f;
-        float startView = 60;
-        float endView = 25;
+        float startView = mainCam.fieldOfView;
+        float endView = decreaseView;
         while (timer < decreaseFieldViewTime)
         {
             timer+=Time.deltaTime;
@@ -55,6 +25,37 @@ public class LastTimeline : MonoBehaviour
             yield return null;
         }
         mainCam.fieldOfView = endView;
+    }
+
+    public void CallReturnFieldOfView() { StartCoroutine(IncreaseFieldOfView()); }
+    IEnumerator IncreaseFieldOfView()
+    {
+        float timer = 0f;
+        float startView = mainCam.fieldOfView;
+        float endView = increaseView;
+        while (timer < decreaseFieldViewTime)
+        {
+            timer += Time.deltaTime;
+            mainCam.fieldOfView = Mathf.Lerp(startView, endView, timer / decreaseFieldViewTime);
+            yield return null;
+        }
+        mainCam.fieldOfView = endView;
+    }
+
+    public void CallLastFadeOut() { StartCoroutine(FadeOut()); }
+    IEnumerator FadeOut()
+    {
+        float timer = 0f;
+        Color stColor = fadeImage.color;
+        stColor.a = 0;
+        while (timer < fadeoutTime)
+        {
+            timer += Time.deltaTime;
+            stColor.a = Mathf.Lerp(0f, 1f, timer / fadeoutTime);
+            fadeImage.color = stColor;
+            yield return null;
+        }
+        fadeImage.color = stColor;
     }
 }
 
