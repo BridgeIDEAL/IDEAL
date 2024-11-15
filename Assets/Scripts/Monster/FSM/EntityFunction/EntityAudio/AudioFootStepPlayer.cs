78d17193d3aeb8fa702b963214dd637611e2c279
hch3245 <hch3245@gmail.com>
2024-11-15 22:31:29 +0900

    이형체 발걸음 소리 층 다르면 안나도록

--- Assets/Scripts/Monster/FSM/EntityFunction/EntityAudio/AudioFootStepPlayer.cs
+++ Assets/Scripts/Monster/FSM/EntityFunction/EntityAudio/AudioFootStepPlayer.cs
@@ -11,11 +11,49 @@ public class AudioFootStepPlayer : AudioSFXPlayer
     int lastRunIndex = -1;
     int footStepCnt = -1;
     int runStepCnt = -1;
+
+    private Coroutine soundCoroutine;
+    private float fadeTime = 1.0f;
+    private float floorInterval = 1.0f;
+    private float footSoundVolume;
+    private bool isPlayerFootHear = false;
+    private Transform playerTransform = null;
+
     protected override void Awake()
     {
         base.Awake();
         footStepCnt=footStepClips.Length;
         runStepCnt = runStepClips.Length;
+        footSoundVolume = source.volume;
+    }
+
+
+    private void Update() {
+        if(playerTransform == null){
+            if(IdealSceneManager.Instance.CurrentGameManager != null ){
+                playerTransform = IdealSceneManager.Instance.CurrentGameManager.scriptHub.playerArmatureObject.transform;
+                return;
+            }
+        }
+        // 앞서 넣더라도 해당 프레임에 position에 접근하면 Null 에러 뜸
+        if(playerTransform == null) return;
+
+
+        if(playerTransform.position.y >= this.transform.position.y - floorInterval && 
+        playerTransform.position.y <= this.transform.position.y + floorInterval){
+            // 플레이어가 이형체와 층 범위가 같을 때
+            if(!isPlayerFootHear){
+                // 이형체의 발소리를 듣고 있지 않다면
+                FootSoundFade(true);
+            }
+        }
+        else{
+            // 플레이어가 이형체와 층 범위가 다를 때
+            if(isPlayerFootHear){
+                // 이형체의 발소리를 듣고 있다면
+                FootSoundFade(false);
+            }
+        }
     }
 
     public void FootStep()
@@ -38,5 +76,27 @@ public class AudioFootStepPlayer : AudioSFXPlayer
         while (randNum == lastRunIndex);
         lastRunIndex = randNum;
         source.PlayOneShot(runStepClips[lastRunIndex]);
+    }
+
+    private void FootSoundFade(bool isFadeIn){
+        if(soundCoroutine != null){
+            StopCoroutine(soundCoroutine);
+        }
+        soundCoroutine = StartCoroutine(FootSoundFadeCoroutine(isFadeIn));
+    }
+
+    private IEnumerator FootSoundFadeCoroutine(bool isFadeIn){
+        isPlayerFootHear = isFadeIn;
+
+        float stepTimer = 0.0f;
+        float soundVol = source.volume;
+        float destVol = isFadeIn ? footSoundVolume : 0.0f;
+
+        while(stepTimer <= fadeTime){
+            source.volume = Mathf.Lerp(soundVol, destVol, stepTimer / fadeTime);
+            stepTimer += Time.deltaTime;
+            yield return null;
+        }
+
     }
 }
