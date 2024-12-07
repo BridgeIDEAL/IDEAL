c9e0c79bf3ce9b4a3ab73a7a91412b0b28dd222d
Sung Jun Cho <chosung0531@naver.com>
2024-12-07 21:53:22 +0900

    진로진학부 메모장 퀘스트 추가
    - 추가적으로 수위 발소리 조정

--- Assets/6_Scripts/UI/Dialogue/EventDataManager.cs
+++ Assets/6_Scripts/UI/Dialogue/EventDataManager.cs
@@ -1,11 +1,12 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class EventDataManager : MonoBehaviour
 {
-    public static EventDataManager Instance;
+    [Header("진로진학부 퀘스트"),SerializeField] TalkDialogueQuest careerQuest;
+    public TalkDialogueQuest CareerQuest { get { return careerQuest; } }
 
+    public static EventDataManager Instance;
     public bool RingAfterSchoolBell { get; set; } = false;
     Dictionary<string, EventData> eventGroup = new Dictionary<string, EventData>();
 
@@ -47,5 +48,54 @@ public class EventDataManager : MonoBehaviour
     {
         if(eventGroup.Count!=0) eventGroup.Clear();
         RingAfterSchoolBell = false;
+        careerQuest?.Init(); 
+    }
+}
+
+
+[System.Serializable]
+public class TalkDialogueQuest
+{
+    Dictionary<string, bool> questDictionary = new Dictionary<string, bool>();
+    [SerializeField] string[] dialogueQuestNames;
+    [SerializeField] bool[] acceptDialogueQuests;
+    public void Init()
+    {
+        Clear();
+        int cnt = dialogueQuestNames.Length;
+        for (int i = 0; i < cnt; i++)
+        {
+            if (questDictionary.ContainsKey(dialogueQuestNames[i]))
+                continue;
+            questDictionary.Add(dialogueQuestNames[i], acceptDialogueQuests[i]);
+        }
+    }
+
+    public void Clear()
+    {
+        if (questDictionary.Count == 0)
+            return;
+        questDictionary.Clear();
+    }
+
+    public void AcceptDialogueQuest(string _questName, bool _acceptDialogueQuest)
+    {
+        if (questDictionary.ContainsKey(_questName))
+        {
+            questDictionary[_questName] = _acceptDialogueQuest;
+        }
+    }
+
+    public bool CheckClearQuest()
+    {
+        List<string> keys = new List<string>(questDictionary.Keys); 
+        int cnt = keys.Count;   
+        for(int i=0; i<cnt; i++)
+        {
+            if (questDictionary[keys[i]] == false)
+                return false;
+        }
+
+        return true;
     }
 }
